38c14755940e47da6390fa976b010075009b542c
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-02-03 18:18:19 +0530

    basic scafolding for harnessing_chaos

---
+++ 2025/1_harnessing_chaos/devenv.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  languages.python = {
+    enable = true;
+    package = pkgs.python313;
+    uv.enable = true;
+    uv.sync.enable = true;
+    venv.enable = true;
+  };
+  packages = with pkgs; [
+    cairo
+    pango
+  ];
+}
